8a5981f1076b91bed5381ccfcd1f219a3be2295f
Samuel Cole <cole.samuel21@gmail.com>
2021-01-23 10:22:14 -0600

    Add lodash, add documentation to utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,25 @@
-export const sortAsc = numArr => [...numArr].sort((a, b) => a - b);
+/**
+ * Sort an array of numbers in ascending order without mutating the original
+ * array.
+ * @param {number[]} numArr
+ * @returns {number[]}
+ */
+export const sortAsc = numArr => [ ...numArr ].sort((a, b) => a - b);
 
+/**
+ * Convert a slider value to a percentage-based position.
+ * @param {number} value
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
 export const valueToPosition = (value, min, max) => (value - min) / (max - min) * 100;
 
+/**
+ * Convert a percentage-based position into a slider value.
+ * @param {number} position
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
 export const positionToValue = (position, min, max) => ((position / 100) * (max - min)) + min;
